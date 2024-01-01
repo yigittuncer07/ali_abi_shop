37bbaca683137ee7e70edfc063bcad32712384bb
Hasan Pekedis <hasanpekedis365@hotmail.com>
2024-01-01 16:45:21 +0300

    final

--- tempIndex.sql
+++ tempIndex.sql
@@ -64,3 +64,126 @@ LEFT JOIN
     Product p ON c.ProductId = p.ProductId
 GROUP BY
     c.Category;
+
+
+CREATE TRIGGER trg_UpdateItemDateBought
+ON Item
+AFTER INSERT AS
+BEGIN
+    UPDATE i
+    SET i.DateBought = GETDATE()
+    FROM Item i
+    INNER JOIN INSERTED ins ON i.ItemId = ins.ItemId;
+END;
+
+--1
+CREATE PROCEDURE GetEmployeeInfo
+    @EmployeeId INT
+AS
+BEGIN
+    SELECT * FROM Employee WHERE EmployeeId = @EmployeeId;
+END;
+
+--2
+CREATE PROCEDURE SearchProductsByPriceRange
+    @MinPrice INT,
+    @MaxPrice INT
+AS
+BEGIN
+    SELECT * FROM Product WHERE Price BETWEEN @MinPrice AND @MaxPrice;
+END;
+
+--3
+CREATE PROCEDURE GetCustomerReceipts
+    @CustomerId INT
+AS
+BEGIN
+    SELECT * FROM Receipt WHERE CustomerId = @CustomerId;
+END;
+
+--4
+CREATE PROCEDURE GetItemsByPurchaseDate
+    @PurchaseDate DATE
+AS
+BEGIN
+    SELECT * FROM Item WHERE DateBought = @PurchaseDate;
+END;
+
+
+--5
+CREATE PROCEDURE SellProduct
+    @ItemId INT,
+    @ReceiptId INT,
+    @SellingPrice INT
+AS
+BEGIN
+    UPDATE Item SET IsSold = 1, DateSold = GETDATE(), SellingPrice = @SellingPrice WHERE ItemId = @ItemId AND ReceiptId = @ReceiptId;
+END;
+
+--6
+CREATE PROCEDURE AddProduct
+    @ProductId INT,
+    @Name VARCHAR(50),
+    @Definition VARCHAR(250),
+    @Price INT
+AS
+BEGIN
+    INSERT INTO Product (ProductId, Name, Definition, Price) VALUES (@ProductId, @Name, @Definition, @Price);
+END;
+
+--7
+CREATE PROCEDURE AddEmployee
+    @EmployeeId INT,
+    @Name VARCHAR(50),
+    @Surname VARCHAR(50),
+    @Password VARCHAR(50),
+    @Email VARCHAR(50),
+    @Salary INT,
+    @WorkEntranceDate DATE,
+    @BirthDate DATE,
+    @PhoneNumber VARCHAR(15),
+    @ManagerId INT
+AS
+BEGIN
+    INSERT INTO Employee (EmployeeId , Name, Surname, Password, Email, Salary, WorkEntranceDate, BirthDate, PhoneNumber, ManagerId)
+    VALUES (@EmployeeId, @Name, @Surname, @Password, @Email, @Salary, @WorkEntranceDate, @BirthDate, @PhoneNumber, @ManagerId);
+END;
+
+--8
+CREATE PROCEDURE AddCustomer
+    @CustomerId INT,
+    @Name VARCHAR(50),
+    @Surname VARCHAR(50),
+    @PhoneNumber VARCHAR(15)
+AS
+BEGIN
+    INSERT INTO Customer (CustomerId, Name, Surname, PhoneNumber)
+    VALUES (@CustomerId, @Name, @Surname, @PhoneNumber);
+END;
+
+
+--9
+CREATE PROCEDURE AddDocument
+    @PrintOrderId INT,
+    @FileContent VARCHAR(250),
+    @Quantity INT
+AS
+BEGIN
+    INSERT INTO Document (FileContent, Quantity, PrintOrderId)
+    VALUES(@FileContent, @Quantity, @PrintOrderId);
+END;
+
+
+--10
+CREATE PROCEDURE AddEmployeeAdress
+    @EmployeeId INT,
+    @Country VARCHAR(50),
+    @City VARCHAR(50),
+    @Street VARCHAR(250),
+    @HouseNumber VARCHAR(50),
+    @ZipCode VARCHAR(50)
+AS
+BEGIN
+    INSERT INTO EmployeeAdress (EmployeeId, Country, City, Street, HouseNumber, ZipCode)
+    VALUES (@EmployeeId, @Country, @City, @Street, @HouseNumber, @ZipCode);
+END;
